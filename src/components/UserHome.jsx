b85d8f5ff16b0c218eaf7e41612f0836b3f2e8c8
Edwin Bravo <edwinbravo2121@gmail.com>
2024-12-04 05:17:23 -0500

    cambios en userhome

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './styles/UserHome.css';
 
 const UserHome = () => {
   const [archivos, setArchivos] = useState([]);
